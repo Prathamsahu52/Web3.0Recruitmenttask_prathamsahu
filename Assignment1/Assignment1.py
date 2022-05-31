e4cc7ea2b364afeef0074aad3be2dd83815cab77
Prathamsahu52 <92453722+Prathamsahu52@users.noreply.github.com>
2022-05-31 10:55:50 +0530

    Add files via upload

---
+++ Assignment1/Assignment1.py
@@ -0,0 +1,31 @@
+import hashlib
+import time
+
+a_string=input("Enter string inside quotes: ")
+
+start_t=time.time()
+hashed_string = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+target="0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+
+n=0
+
+
+
+while(hashed_string>=target):
+    a_string=a_string+str(n)
+    n=n+1
+    hashed_string = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+    
+
+
+    
+
+
+print(hashed_string)
+print(n)
+end_t=time.time()
+
+print(end_t-start_t)
+
+
+
