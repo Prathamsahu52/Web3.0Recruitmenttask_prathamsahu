690cf1feb4a0c3dd997fa52516c2e5fa7c9b7aaa
Prathamsahu52 <92453722+Prathamsahu52@users.noreply.github.com>
2022-05-31 14:17:35 +0530

    Update Assignment1.py

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -4,20 +4,20 @@ import time
 a_string=input("Enter string inside quotes: ")
 
 start_t=time.time()
-hashed_string = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+hashed_string = hashlib.sha256(a_string.encode()).hexdigest()
 target='0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'
 
 n=0
 
 
 while(int(hashed_string,16)>=int(target,16)):
-    a_string=a_string+str(n)
+    b_string=a_string+str(n)
     n=n+1
-    hashed_string = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+    hashed_string = hashlib.sha256(b_string.encode()).hexdigest()
     
 
-print(hashed_string)
-print(n)
+print("hashed ", hashed_string)
+print("nonce ",n-1)
 end_t=time.time()
 
-print(end_t-start_t)
+print("time taken ",end_t-start_t)
